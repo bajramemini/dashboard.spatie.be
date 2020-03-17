731c571370f1e001332b5d809a45811f8d6adb25
Bajram Emini <bajram@eminiarts.ch>
2020-03-17 22:00:43 +0100

    Update TeamworkApiController.php

--- app/Http/Controllers/TeamworkApiController.php
+++ app/Http/Controllers/TeamworkApiController.php
@@ -14,6 +14,55 @@ class TeamworkApiController
         $response = Http::withBasicAuth($token, 'X')->get($url . '/latestActivity.json');
 
         return $response->json();
-
     }
+
+    public function projects()
+    {
+        $url   = config('services.teamwork.url');
+        $token = config('services.teamwork.token');
+
+        $response = Http::withBasicAuth($token, 'X')->get($url . '/projects.json');
+
+        return $response->json();
+    }
+
+    public function project($id)
+    {
+        $url   = config('services.teamwork.url');
+        $token = config('services.teamwork.token');
+
+        $response = Http::withBasicAuth($token, 'X')->get($url . '/projects/' . $id . '.json');
+
+        return $response->json();
+    }
+
+    public function milestones()
+    {
+        $url   = config('services.teamwork.url');
+        $token = config('services.teamwork.token');
+
+        $response = Http::withBasicAuth($token, 'X')->get($url . '/milestones.json');
+
+        return $response->json();
+    }
+    public function milestone($id)
+    {
+        $url   = config('services.teamwork.url');
+        $token = config('services.teamwork.token');
+
+        $response = Http::withBasicAuth($token, 'X')->get($url . '/milestone/' . $id . '.json');
+
+        return $response->json();
+    }
+
+    public function boardColumn($id)
+    {
+        $url   = config('services.teamwork.url');
+        $token = config('services.teamwork.token');
+
+        $response = Http::withBasicAuth($token, 'X')->get($url . '/boards/columns/' . $id . '/cards.json/');
+
+        return $response->json();
+    }
+
 }
