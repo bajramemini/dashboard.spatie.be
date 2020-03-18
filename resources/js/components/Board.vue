754448005a3c283128e77d81fc32367fd49e9fca
Bajram Emini <bajram@eminiarts.ch>
2020-03-18 18:48:55 +0100

    WIP

--- resources/js/components/Board.vue
+++ resources/js/components/Board.vue
@@ -5,16 +5,15 @@
             <ul class>
                 <li v-for="task in tasks">
                     <div class="my-2 text-sm">
-                        <li class="bg-gray-900 rounded mb-2 p-4">
-                            <small class="text-gray-500" v-for="user in task.users">
-                                <img :src="user.avatarUrl" alt class="w-4 h-4 rounded-full" />
-                                {{ user.firstName }}
-                            </small>
+                        <li class="bg-gray-900 rounded mb-2 p-4 overflow-hidden">
                             <strong class="hidden">{{ task.progress}}</strong>
                             {{ task.name }}
-                            <br />
-
                             <small>{{ task.created_at }}</small>
+
+                            <small class="flex mt-2" v-for="user in task.users">
+                                <img :src="user.avatarUrl" alt class="w-4 h-4 rounded-full mr-2" />
+                                <span class="opacity-25">{{ user.firstName }}</span>
+                            </small>
                         </li>
                     </div>
                 </li>
